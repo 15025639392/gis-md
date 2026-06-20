d77fad68bdd233a624bf92c58d27fd9acfd3703d
ldy <54ldyy@gmail.com>
2026-06-21 05:15:01 +0800

    Split attribute compression tests

---
+++ scaffold/tests/unit/core/test_attribute_compression.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+
+#include "earth_engine/core/math/AttributeCompression.h"
+#include "earth_engine/core/math/MathUtils.h"
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+#include <glm/ext/vector_double3.hpp>
+#include <glm/geometric.hpp>
+
+using namespace earth_engine;
+
+namespace {
+
+bool equalsEpsilon(const glm::dvec3& left,
+                   const glm::dvec3& right,
+                   double epsilon) {
+    return MathUtils::equalsEpsilon(left.x, right.x, epsilon) &&
+           MathUtils::equalsEpsilon(left.y, right.y, epsilon) &&
+           MathUtils::equalsEpsilon(left.z, right.z, epsilon);
+}
+
+} // namespace
+
+TEST(AttributeCompressionTest, OctDecodeMatchesCesiumNative) {
+    const std::array<std::pair<uint8_t, uint8_t>, 14> input{{
+        {128, 128},
+        {255, 255},
+        {128, 255},
+        {128, 0},
+        {255, 128},
+        {0, 128},
+        {170, 170},
+        {170, 85},
+        {85, 85},
+        {85, 170},
+        {213, 213},
+        {213, 42},
+        {42, 42},
+        {42, 213},
+    }};
+    const std::array<glm::dvec3, 14> expected{{
+        glm::dvec3(0.0, 0.0, 1.0),
+        glm::dvec3(0.0, 0.0, -1.0),
+        glm::dvec3(0.0, 1.0, 0.0),
+        glm::dvec3(0.0, -1.0, 0.0),
+        glm::dvec3(1.0, 0.0, 0.0),
+        glm::dvec3(-1.0, 0.0, 0.0),
+        glm::normalize(glm::dvec3(1.0, 1.0, 1.0)),
+        glm::normalize(glm::dvec3(1.0, -1.0, 1.0)),
+        glm::normalize(glm::dvec3(-1.0, -1.0, 1.0)),
+        glm::normalize(glm::dvec3(-1.0, 1.0, 1.0)),
+        glm::normalize(glm::dvec3(1.0, 1.0, -1.0)),
+        glm::normalize(glm::dvec3(1.0, -1.0, -1.0)),
+        glm::normalize(glm::dvec3(-1.0, -1.0, -1.0)),
+        glm::normalize(glm::dvec3(-1.0, 1.0, -1.0)),
+    }};
+
+    for (size_t i = 0; i < expected.size(); ++i) {
+        const glm::dvec3 value =
+            AttributeCompression::octDecode(input[i].first, input[i].second);
+        EXPECT_TRUE(equalsEpsilon(value, expected[i], MathUtils::Epsilon1))
+            << "index " << i;
+    }
+}
+
+TEST(AttributeCompressionTest, DecodeRGB565MatchesCesiumNative) {
+    const std::array<uint16_t, 4> input{{
+        0u,
+        2081u,
+        33800u,
+        65535u,
+    }};
+    const std::array<glm::dvec3, 4> expected{{
+        glm::dvec3(0.0),
+        glm::dvec3(1.0 / 31.0, 1.0 / 63.0, 1.0 / 31.0),
+        glm::dvec3(16.0 / 31.0, 32.0 / 63.0, 8.0 / 31.0),
+        glm::dvec3(1.0),
+    }};
+
+    for (size_t i = 0; i < expected.size(); ++i) {
+        const glm::dvec3 value = AttributeCompression::decodeRGB565(input[i]);
+        EXPECT_TRUE(equalsEpsilon(value, expected[i], MathUtils::Epsilon6))
+            << "index " << i;
+    }
+}
--- scaffold/tests/unit/core/test_math_utils.cpp
+++ scaffold/tests/unit/core/test_math_utils.cpp
@@ -1,28 +1,13 @@
 #include <gtest/gtest.h>
 
-#include "earth_engine/core/math/AttributeCompression.h"
 #include "earth_engine/core/math/MathUtils.h"
 #include "earth_engine/core/math/Mat4.h"
 #include "earth_engine/core/math/Vec3.h"
 
-#include <array>
 #include <cmath>
-#include <cstddef>
 #include <limits>
 
 using namespace earth_engine;
-
-namespace {
-
-bool equalsEpsilon(const glm::dvec3& left,
-                   const glm::dvec3& right,
-                   double epsilon) {
-    return MathUtils::equalsEpsilon(left.x, right.x, epsilon) &&
-           MathUtils::equalsEpsilon(left.y, right.y, epsilon) &&
-           MathUtils::equalsEpsilon(left.z, right.z, epsilon);
-}
-
-} // namespace
 
 TEST(MathUtilsTest, ConstantsAndAngleConversionsMatchCesiumNative) {
     EXPECT_DOUBLE_EQ(1e-1, MathUtils::Epsilon1);
@@ -42,69 +27,6 @@ TEST(MathUtilsTest, LerpMatchesCesiumNative) {
     EXPECT_DOUBLE_EQ(1.0, MathUtils::lerp(1.0, 2.0, 0.0));
     EXPECT_DOUBLE_EQ(1.5, MathUtils::lerp(1.0, 2.0, 0.5));
     EXPECT_DOUBLE_EQ(2.0, MathUtils::lerp(1.0, 2.0, 1.0));
-}
-
-TEST(AttributeCompressionTest, OctDecodeMatchesCesiumNative) {
-    const std::array<std::pair<uint8_t, uint8_t>, 14> input{{
-        {128, 128},
-        {255, 255},
-        {128, 255},
-        {128, 0},
-        {255, 128},
-        {0, 128},
-        {170, 170},
-        {170, 85},
-        {85, 85},
-        {85, 170},
-        {213, 213},
-        {213, 42},
-        {42, 42},
-        {42, 213},
-    }};
-    const std::array<glm::dvec3, 14> expected{{
-        glm::dvec3(0.0, 0.0, 1.0),
-        glm::dvec3(0.0, 0.0, -1.0),
-        glm::dvec3(0.0, 1.0, 0.0),
-        glm::dvec3(0.0, -1.0, 0.0),
-        glm::dvec3(1.0, 0.0, 0.0),
-        glm::dvec3(-1.0, 0.0, 0.0),
-        glm::normalize(glm::dvec3(1.0, 1.0, 1.0)),
-        glm::normalize(glm::dvec3(1.0, -1.0, 1.0)),
-        glm::normalize(glm::dvec3(-1.0, -1.0, 1.0)),
-        glm::normalize(glm::dvec3(-1.0, 1.0, 1.0)),
-        glm::normalize(glm::dvec3(1.0, 1.0, -1.0)),
-        glm::normalize(glm::dvec3(1.0, -1.0, -1.0)),
-        glm::normalize(glm::dvec3(-1.0, -1.0, -1.0)),
-        glm::normalize(glm::dvec3(-1.0, 1.0, -1.0)),
-    }};
-
-    for (size_t i = 0; i < expected.size(); ++i) {
-        const glm::dvec3 value =
-            AttributeCompression::octDecode(input[i].first, input[i].second);
-        EXPECT_TRUE(equalsEpsilon(value, expected[i], MathUtils::Epsilon1))
-            << "index " << i;
-    }
-}
-
-TEST(AttributeCompressionTest, DecodeRGB565MatchesCesiumNative) {
-    const std::array<uint16_t, 4> input{{
-        0u,
-        2081u,
-        33800u,
-        65535u,
-    }};
-    const std::array<glm::dvec3, 4> expected{{
-        glm::dvec3(0.0),
-        glm::dvec3(1.0 / 31.0, 1.0 / 63.0, 1.0 / 31.0),
-        glm::dvec3(16.0 / 31.0, 32.0 / 63.0, 8.0 / 31.0),
-        glm::dvec3(1.0),
-    }};
-
-    for (size_t i = 0; i < expected.size(); ++i) {
-        const glm::dvec3 value = AttributeCompression::decodeRGB565(input[i]);
-        EXPECT_TRUE(equalsEpsilon(value, expected[i], MathUtils::Epsilon6))
-            << "index " << i;
-    }
 }
 
 TEST(MathUtilsTest, EqualsEpsilonMatchesCesiumNative) {
