14e2ac9c3c5a71ff75bbcc86e897e0335e45c580
ldy <54ldyy@gmail.com>
2026-06-21 17:30:02 +0800

    Cover tile render entry roles

---
+++ scaffold/tests/unit/tiling/test_tile_render_entry.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include "earth_engine/tiling/TilePlan.h"
+
+using namespace earth_engine;
+
+TEST(TileRenderEntryTest, DirectEntryHasSelectedPassOnly) {
+    TileRenderEntry entry;
+    entry.reason = TileRenderEntryReason::Direct;
+
+    EXPECT_FALSE(entry.isAncestorFallback());
+    EXPECT_FALSE(entry.isFadingOut());
+    EXPECT_FALSE(entry.hasSurfaceClip());
+    EXPECT_EQ(entry.renderPass(), TileRenderEntryPass::Selected);
+}
+
+TEST(TileRenderEntryTest, FallbackEntryExposesClippedAncestorRole) {
+    TileRenderEntry entry;
+    entry.reason = TileRenderEntryReason::AncestorFallback;
+    entry.usesAncestorFallback = true;
+    entry.surfaceClipEnabled = true;
+
+    EXPECT_TRUE(entry.isAncestorFallback());
+    EXPECT_FALSE(entry.isFadingOut());
+    EXPECT_TRUE(entry.hasSurfaceClip());
+    EXPECT_EQ(entry.renderPass(), TileRenderEntryPass::Selected);
+}
+
+TEST(TileRenderEntryTest, FadingEntryUsesFadingPass) {
+    TileRenderEntry entry;
+    entry.reason = TileRenderEntryReason::FadingOut;
+    entry.selectedThisFrame = false;
+
+    EXPECT_FALSE(entry.isAncestorFallback());
+    EXPECT_TRUE(entry.isFadingOut());
+    EXPECT_FALSE(entry.hasSurfaceClip());
+    EXPECT_EQ(entry.renderPass(), TileRenderEntryPass::Fading);
+}
